da6a22ac68246345b7383c21fa0d53d3a3d1aa7d
Emma Nuss <enuss@CEEs-MacBook-Pro.local>
2021-01-27 14:52:55 -0800

    saved figures, updated code, and added report

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -6,13 +6,16 @@ from mpl_toolkits.mplot3d import Axes3D
 import plotly.graph_objects as go
 plt.ion()
 
+# definte data path and filename
 datapath = '../data/'
 filename = 'subdata.csv'
 
+# load in data and reformat complex numbers for python
 subdata = pd.read_csv(os.path.join(datapath, filename), header=None)
 subdata = subdata.applymap(lambda s: np.complex(s.replace('i', 'j')))
 data = subdata.values
 
+# define spatial and frequency domains 
 L = 10
 n = 64
 k_pos = np.arange(0, n/2)
@@ -24,14 +27,13 @@ x2 = np.linspace(-L,L,n+1)
 x = x2[:n]
 y = x
 z = x
-[X, Y, Z] = np.meshgrid(x,y,z)
-[Kx,Ky,Kz] = np.meshgrid(ks,ks,ks)
+[X, Y, Z] = np.meshgrid(x,y,z, indexing='ij')
+[Kx,Ky,Kz] = np.meshgrid(ks,ks,ks, indexing='ij')
 
-Un = np.asarray([np.reshape(data[:,i], (n,n,n)) for i in range(data.shape[-1])])
-#M = np.max(np.abs(Un))
+# reshape data into 4-d time-spatial data array
+Un = np.asarray([np.reshape(data[:,i], (n,n,n), order='F') for i in range(data.shape[-1])])
 
-
-### finding the frequency
+# find the frequency of the submarine
 Un_fft = np.fft.fftshift(np.fft.fftn(Un, axes=[1,2,3]), axes=[1,2,3])
 Un_avg = np.mean(Un_fft, axis=0, dtype=complex)
 Un_avg_normalized = np.abs(Un_avg)/np.max(np.abs(Un_avg))
@@ -48,47 +50,104 @@ kx_avg = np.mean(kx)
 ky_avg = np.mean(ky)
 kz_avg = np.mean(kz)
 
+if 0: # plotting code for isosurface of normalized and averaged data
+	fig = go.Figure(data = go.Isosurface(
+					x=Kx.flatten(),
+					y=Ky.flatten(),
+					z=Kz.flatten(),
+					value=Un_avg_normalized.flatten(),
+					isomin=0.7,
+    				isomax=1,
+    				surface_count=15,
+    				opacity=0.3))
+	fig.update_layout(scene = dict(
+                    xaxis_title='kx',
+                    yaxis_title='ky',
+                    zaxis_title='kz'))
+	fig.show()
+
 kx_centered = (Kx - kx_avg)**2
 ky_centered = (Ky - ky_avg)**2
 kz_centered = (Kz - kz_avg)**2
 
+# creating gaussian filter
 tau = 0.1
 
 gaussian_filter = np.exp(-tau * kx_centered)*np.exp(-tau * ky_centered)*np.exp(-tau * kz_centered)
 
+if 0: # plotting gaussian filter
+	fig = go.Figure(data = go.Isosurface(
+					x=Kx.flatten(),
+					y=Ky.flatten(),
+					z=Kz.flatten(),
+					value=gaussian_filter.flatten(),
+					isomin=0.1,
+    				isomax=1,
+    				surface_count=15,
+    				opacity=0.3))
+	fig.update_layout(scene = dict(
+                    xaxis_title='kx',
+                    yaxis_title='ky',
+                    zaxis_title='kz'))
+	fig.show()
 
-### filtering the data with a gaussian filter at the frequency we found prior
+# filtering the data with a gaussian filter at the frequency we found prior
 Un_fft_filtered = np.asarray([Un_fft[i,:,:,:]*gaussian_filter for i in range(len(Un))])
 
 Un_filtered = np.fft.ifftn(np.fft.ifftshift(Un_fft_filtered, axes=[1,2,3]), axes=[1,2,3])
 Un_filtered_normalized = np.asarray([np.abs(Un_filtered[i,:,:,:])/np.max(np.abs(Un_filtered[i,:,:,:])) for i in range(len(Un_filtered))])
 
-## find locations at each time step that have the max signal 
-#sub_ind = np.squeeze(np.asarray([np.asarray(np.argwhere(Un_filtered[i,:,:,:]==np.max(Un_filtered[i,:,:,:]))) for i in range(len(Un_filtered))]))
+# find locations at each time step that have signal above threshold 
 sub_ind = np.asarray([np.mean(np.asarray(np.argwhere(Un_filtered_normalized[i,:,:,:]>=threshold)), axis=0) for i in range(len(Un_filtered_normalized))])
 
-
+# finding the center of mass of the signal 
 sub_x = np.asarray([x[int(sub_ind[i,0])] for i in range(len(sub_ind))])
 sub_y = np.asarray([y[int(sub_ind[i,1])] for i in range(len(sub_ind))])
 sub_z = np.asarray([z[int(sub_ind[i,2])] for i in range(len(sub_ind))])
 
+if 0: # plotting submarine path
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.plot(sub_x, sub_y, sub_z, color='black')
+	ax.plot(sub_x[-1], sub_y[-1], sub_z[-1], 'o', color='tab:blue')
+	ax.plot(sub_x[0], sub_y[0], sub_z[0], 'o', color='tab:green')
+	ax.set_xlabel('X')
+	ax.set_ylabel('Y')
+	ax.set_zlabel('Z')
+	ax.set_xlim((-L,L))
+	ax.set_ylim((-L,L))
+	ax.set_zlim((-L,L))
+	ax.locator_params(nbins=5)
+	fig.savefig('sub_path.png', transparent=True)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot(sub_x, sub_y, sub_z, label='parametric curve')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('Submarine Path')
-
+if 0: # plotting aircraft path
+	plt.style.use('ggplot')
+	fig, ax = plt.subplots()
+	ax.plot(sub_x, sub_y, color='black')
+	ax.plot(sub_x[-1], sub_y[-1], 'o', color='tab:blue')
+	ax.plot(sub_x[0], sub_y[0], 'o', color='tab:green')
+	ax.set_xlabel('X')
+	ax.set_ylabel('Y')
+	ax.set_xlim((-L,L))
+	ax.set_ylim((-L,L))
+	fig.savefig('air_path.png', transparent=True)
 
 if 0:
-	fig = go.Figure(data = go.Isosurface(
-					x=Kx.flatten(),
-					y=Ky.flatten(),
-					z=Kz.flatten(),
-					value=gaussian_filter.flatten()))
+	# write x, y, z coordinates to csv for table generation
+	f = open('xyz_coords.csv', 'w')
+	subx = sub_x[::4]
+	suby = sub_y[::4]
+	subz = sub_z[::4]
+	for i in range(len(subx)-1):
+		f.write("%0.1f," % (subx[i]))
+	f.write("%0.1f\n" % (subx[-1]))
+	for i in range(len(suby)-1):
+		f.write("%0.1f," % (suby[i]))
+	f.write("%0.1f\n" % (suby[-1]))	
+	for i in range(len(subz)-1):
+		f.write("%0.1f," % (subz[i]))
+	f.write("%0.1f\n" % (subz[-1]))	
+	f.close()
 
-	fig.show()
 
 
